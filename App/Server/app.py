3eec91e8af80f263c18d8d12e964eaabc1c6973b
Ruy-GC <ruy.guzman2002@gmail.com>
2022-09-06 19:30:35 -0500

    json management using local file

--- App/Server/app.py
+++ App/Server/app.py
@@ -1,15 +1,28 @@
-from crypt import methods
-from flask import Flask, request
-from flask_json import FlaskJSON
+from flask import Flask, request,json
+from flask_json import FlaskJSON, json_response, as_json
 
 app = Flask(__name__)
-json = FlaskJSON(app)
+JSON = FlaskJSON(app)
+
+@app.route("/agentpy_json", methods = ['GET'])
+@as_json
+def agentpy_get_data():
+    agents = json.load(open('./data.json'))
+    return json_response(data = agents )
+    
+
 
 @app.route("/agentpy_json", methods = ['POST'])
 def agentpy_send_data():
+
     data = request.get_json(force=True)
-    print(data)
-    return "<p>Hello, World!</p>"
+
+    with open('./data.json', 'w') as outfile:
+        outfile.write(json.dumps(data, indent = 4))
+    
+    outfile.close()
+
+    return "Data Loaded correctly from agentpy"
 
 if __name__ == '__main__':
     app.run()
